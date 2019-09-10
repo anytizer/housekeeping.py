cacd328ce4540310b8b10fc28f064cc498e0f55c
anytizer <anytizer@bimal.org.np>
2019-09-10 00:25:33 -0600

    formatting

--- sqlite/missing.sql
+++ sqlite/missing.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "missing" (
-                             "id"	TEXT NOT NULL,
-                             "created_on"	TEXT NOT NULL,
-                             "associate"	TEXT NOT NULL,
-                             "room_number"	TEXT NOT NULL,
-                             "missingstuffs"	TEXT NOT NULL,
-                             "anc"	TEXT NOT NULL,
-                             "remarks"	TEXT NOT NULL,
-                             "date"	TEXT NOT NULL,
-                             "deleted"	NUMERIC NOT NULL,
-                             PRIMARY KEY("id")
-  );
+	"id"	TEXT NOT NULL,
+	"created_on"	TEXT NOT NULL,
+	"associate"	TEXT NOT NULL,
+	"room_number"	TEXT NOT NULL,
+	"missingstuffs"	TEXT NOT NULL,
+	"anc"	TEXT NOT NULL,
+	"remarks"	TEXT NOT NULL,
+	"date"	TEXT NOT NULL,
+	"deleted"	NUMERIC NOT NULL,
+	PRIMARY KEY("id")
+);
