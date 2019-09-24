34cd5dff4972973d2626f02d7d167371aa58e83b
anytizer <anytizer@bimal.org.np>
2019-09-23 21:09:40 -0600

    spelling error fix

--- importer.py
+++ importer.py
@@ -1,5 +1,6 @@
 import uuid
 import datetime
+import sys
 import sqlite3
 
 source = sqlite3.connect("RawFile.db")
@@ -11,11 +12,13 @@ data = cursor.fetchall()
 source.commit()
 source.close()
 #print(data)
-#print(data[0])
-print("Completed reading the source...");
+print("Data 0th: ", data[0])
+print("Completed reading the source...")
 print("Found entries: ", len(data))
 
-last_associate = list(data[0])[2]
+#sys.exit()
+
+last_associate = list(data[0])[1]
 
 destination = sqlite3.connect("housekeeping.db")
 dcursor = destination.cursor()
@@ -33,46 +36,47 @@ INSERT INTO missing (
 );"""
 for raw1 in data:
     raw = list(raw1)
-    # (0, 1, 'Trina', 122, 'Tea', '', '2019-08-01')
+    # (0, 1, 'Trina', 122, 'Tea', '', '2019-08-01') -- version 1
+    # (1, 'Annie ', 161, 'Pen', None, None, '2019-06-01', '2019-06.xls') -- version 2
 
-    # replace null with ""
+    # replace null field with ""
     for r in range(0, len(raw)):
         raw[r] = str(raw[r]).strip() if raw[r] else ""
 
-    if raw[2] == "":
-        raw[2] = last_associate
+    if raw[1] == "":
+        raw[1] = last_associate
     else:
-        last_associate = raw[2]
+        last_associate = raw[1]
     
     final = [
         str(uuid.uuid4()).upper(),
         str(datetime.datetime.today()),
-        raw[2], # associate
-        raw[3], # room number
-        raw[4], # missingstuffs
-        raw[5], # anc
-        "", # remarks
+        raw[1], # associate
+        raw[2], # room number
+        raw[3], # missingstuffs
+        raw[4], # anc
+        raw[5], # remarks
         raw[6], # date
         0, # deleted
     ]
     dcursor.execute(insert_sql, final)
 
 print("Inserting the records...")
-
-# Adjustments
-# @todo Use fixer.sql
-dcursor.execute("UPDATE missing SET missingstuffs='Pens' WHERE missingstuffs='Pen';")
-dcursor.execute("UPDATE missing SET missingstuffs='Bath Mat' WHERE missingstuffs='bath mat';")
-dcursor.execute("UPDATE missing SET missingstuffs='Ice Bag' WHERE missingstuffs='Ice bag';")
-dcursor.execute("UPDATE missing SET missingstuffs='Ice Bag' WHERE missingstuffs='ice bag';")
-dcursor.execute("UPDATE missing SET missingstuffs='Garbage Bag' WHERE missingstuffs='Garbage bag';")
-dcursor.execute("UPDATE missing SET missingstuffs='Memo Pad (Note Book)' WHERE missingstuffs='Memo pad';")
-dcursor.execute("UPDATE missing SET missingstuffs='Memo Pad (Note Book)' WHERE missingstuffs='Memo Pad';")
-dcursor.execute("UPDATE missing SET missingstuffs='Toilet Paper' WHERE missingstuffs='toilet paper';")
-dcursor.execute("UPDATE missing SET missingstuffs='Toilet Paper' WHERE missingstuffs='Toilet paper';")
-dcursor.execute("UPDATE missing SET missingstuffs='Face Clothes (Towel)' WHERE missingstuffs='Face clothes';")
-
-dcursor.execute("UPDATE missing SET associate='Allyn' WHERE associate='allyn';");
+#
+# # Adjustments
+# # @todo Use fixer.sql
+# dcursor.execute("UPDATE missing SET missingstuffs='Pens' WHERE missingstuffs='Pen';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Bath Mat' WHERE missingstuffs='bath mat';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Ice Bag' WHERE missingstuffs='Ice bag';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Ice Bag' WHERE missingstuffs='ice bag';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Garbage Bag' WHERE missingstuffs='Garbage bag';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Memo Pad (Note Book)' WHERE missingstuffs='Memo pad';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Memo Pad (Note Book)' WHERE missingstuffs='Memo Pad';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Toilet Paper' WHERE missingstuffs='toilet paper';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Toilet Paper' WHERE missingstuffs='Toilet paper';")
+# dcursor.execute("UPDATE missing SET missingstuffs='Face Clothes (Towel)' WHERE missingstuffs='Face clothes';")
+#
+# dcursor.execute("UPDATE missing SET associate='Allyn' WHERE associate='allyn';");
 
 # SELECT missingstuffs, COUNT(missingstuffs) total FROM missing GROUP BY missingstuffs ORDER BY missingstuffs;
 
